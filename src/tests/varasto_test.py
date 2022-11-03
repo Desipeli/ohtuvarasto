371f1f00f43c3b5fdc267cad3108a81b54851805
Desipeli <mr.desipeli@gmail.com>
2022-11-03 12:28:13 +0200

    testaus

--- src/tests/varasto_test.py
+++ src/tests/varasto_test.py
@@ -38,3 +38,39 @@ class TestVarasto(unittest.TestCase):
 
         # varastossa pitäisi olla tilaa 10 - 8 + 2 eli 4
         self.assertAlmostEqual(self.varasto.paljonko_mahtuu(), 4)
+
+    def test_negatiivinen_lisays(self):
+        self.varasto.lisaa_varastoon(-1)
+        # Mikään ei muutu
+        self.assertAlmostEqual(self.varasto.paljonko_mahtuu(), 10)
+    
+    def test_lisaa_liikaa(self):
+        self.varasto.lisaa_varastoon(11)
+        # varasto on nyt täynnä
+        self.assertAlmostEqual(self.varasto.saldo, 10)
+    
+    def test_otetaan_negatiivinen(self):
+        otettu = self.varasto.ota_varastosta(-3)
+        self.assertAlmostEqual(otettu, 0)
+    
+    def test_ei_oteta_enempaa_kuin_voi(self):
+        self.varasto.lisaa_varastoon(8)
+        otettu = self.varasto.ota_varastosta(10)
+
+        self.assertAlmostEqual(otettu, 8)
+        self.assertAlmostEqual(self.varasto.paljonko_mahtuu(), 10)
+    
+    def test_merkkijonoesitys(self):
+        self.varasto.lisaa_varastoon(6)
+        self.assertAlmostEqual(
+            self.varasto.__str__(),
+            "saldo = 6, vielä tilaa 4"
+        )
+    
+    def test_luodaan_varasto_negatiivisella_tilavuudella(self):
+        toinen_varasto = Varasto(-1)
+        self.assertAlmostEqual(toinen_varasto.tilavuus, 0)
+    
+    def test_negatiivinen_alku_saldo(self):
+        toinen_varasto = Varasto(5, -1)
+        self.assertAlmostEqual(toinen_varasto.saldo, 0)
